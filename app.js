730367010f73d17a4af94271e62fa4cef0e7f698
olikelly00 <oliverkelly1995@hotmail.co.uk>
2024-07-23 14:20:20 +0100

    user story 3, initial work

--- app.js
+++ app.js
@@ -14,4 +14,20 @@ app.listen(port, () => {
     console.log(`We are now listening on port ${port}`)
 })
 
+
+app.get("/activities/:id", (req,res) => {
+
+    //const { activity } = res.body
+
+    res.send(
+        {
+            "id": "54321234", // UUID
+            "activity_submitted": "1719486190058", // simple Epoc timestamp (Date.now() in JS)
+            "activity_type": "run", // choose some standard types
+            "activity_duration": "30", // choose standard unit type (minutes probably)
+        }
+        )
+    });
+
+
 //http://localhost:3000/
