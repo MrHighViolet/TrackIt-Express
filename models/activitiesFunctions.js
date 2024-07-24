f989136ea448509c6f27238c7b5375c0b7f46524
olikelly00 <oliverkelly1995@hotmail.co.uk>
2024-07-24 16:05:59 +0100

    amended getactivities function

--- models/activitiesFunctions.js
+++ models/activitiesFunctions.js
@@ -1,18 +1,22 @@
 import activities from "../libs/activities.js"; 
 
-
 export default async function getActivitiesByID(number) { 
     const resultList = [];
     
     for (let i = 0; i < activities.length; i++) {
+      
+      
         if (activities[i].id == number) {
             resultList.push(activities[i]);
         }
-    if (resultList.length > 0) {
-      return resultList;
+      }
+        console.log(resultList)
+    if (resultList.length === 0) {
+      throw new Error(`No activity with ${number} found.`);
     }
-    throw new Error(`No activity with ${number} found.`);
-  };
+    
+  
+  return resultList;
 };
 
 // console.log(getActivitiesByID(74321234));
