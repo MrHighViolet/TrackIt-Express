db4266eab01917e01e55f36079f899ab41cd747e
Jonathan Hill <144107180+MrHighViolet@users.noreply.github.com>
2024-07-24 14:07:01 +0100

    created activitiesFunctions.js and activities.js and created getActivitesById function

---
+++ models/activitiesFunctions.js
@@ -0,0 +1,15 @@
+import activities from "../libs/activities.js" 
+
+
+export async function getActivitiesByID(number) { 
+    const resultList = [];
+    
+    for (let i = 0; i < activities.length; i++) {
+        if (activities[i].id == number) {
+            resultList.push(activities[i]);
+        }
+    }
+    return resultList;
+}
+
+console.log(getActivitiesByID(74321234));
