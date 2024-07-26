94c3b5937b1a2cca61635512913b8f55303b1b66
Jonathan Hill <144107180+MrHighViolet@users.noreply.github.com>
2024-07-26 11:59:34 +0100

    clearned up updateActivity function

--- models/activitiesFunctions.js
+++ models/activitiesFunctions.js
@@ -50,31 +50,32 @@ export async function addNewActivity(newActivity) {
 /* This function returns an existing activity with its properties updated according to the user's input */
 
 export async function updateActivity(updates) {
-  let index = null
-  const activityID = 'id' in updates
+  let index = null;
+  const activityID = 'id' in updates;
+  
   if (!activityID){
       throw new Error(`You haven't given us an id.`);
   }
   for (let i = 0; i < activities.length; i++) {
     if (activities[i].id == updates.id) {
-         index = i
+         index = i;
     }
   }
   if (index === null) {
       throw new Error(`No activity ID with ${updates.id} found.`);
     }
   
-  const activityType = 'activity_type' in updates
+  const activityType = 'activity_type' in updates;
   if (activityType) {
-    activities[index].activity_type = updates.activity_type
+    activities[index].activity_type = updates.activity_type;
   }
-  const activityDuration = 'activity_duration' in updates
+  const activityDuration = 'activity_duration' in updates;
   if (activityDuration) {
-    activities[index].activity_duration = updates.activity_duration
+    activities[index].activity_duration = updates.activity_duration;
   }
-  activities[index].activity_submitted = Date.now()
+  activities[index].activity_submitted = Date.now();
 
-  return activities[index]
+  return activities[index];
 };
 
 /* This function deletes an existing activity with the specified id from the list and returns the deleted activity */
